ed3797f8d2522cb1bbace519632736e8ed394599
Irakli <koregm@gmail.com>
2021-11-23 18:02:50 +0400

    First commit

---
+++ first-app.js
@@ -0,0 +1,7 @@
+// console.log("Hello from node.js");
+
+// import for working wih file system
+const fs = require("fs");
+
+// creates file, with name and content
+fs.writeFileSync("hello.txt ", "Hello from node.js");
